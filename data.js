96f62eb0a51b01098a8c8278e612b5033c78b884
sridhar-dev <85818985+sridhar-dev@users.noreply.github.com>
2021-11-25 01:09:59 +0530

    4675
    Done :-)

--- data.js
+++ data.js
@@ -1,1 +1,10 @@
-
+const userDatas=(id,name,year,qualification)=>{
+    data={
+        id:id,
+        name:name,
+        year:year,
+        qualification:qualification
+    }
+    return data;
+}
+module.exports=userDatas
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const datas=require('./data');
+const os=require('os');
+const fs=require('fs');
+const crypto = require('crypto');
 
+const getos=os.userInfo();
+
+fs.writeFileSync('message.txt',' message.txt')
+const userdatas=datas(4675,"Prograd",2021,"B.E");
+const password = crypto.createHmac('sha256','a secret').update('Prograd').digest('hex');
+fs.appendFileSync('message.txt',`\n Prograd Details
+    UserId ->        ${userdatas.id},   
+    Name ->          ${userdatas.name},
+    Year ->          ${userdatas.year},
+    Qualification -> ${userdatas.qualification},
+    Username ->      ${getos.username},
+    Password ->      ${password}`)
